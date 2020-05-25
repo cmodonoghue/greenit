779e720656cc0623f25b4cd6daeaf9c4b1f7c2b8
Christian <cmodonoghue@crimson.ua.edu>
2020-05-25 12:07:35 -0500

    edit header

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,23 +1,10 @@
 import React, { useState } from "react";
 
 const Header = () => {
-  const [enlightenment, becomeEnlightened] = useState("Sign in");
-
-  const proclaimFreedom = () => {
-    becomeEnlightened("No need to sign in. Greenit is about freedom.");
-  };
 
   return (
     <div className="header">
       <h1 className="header__title">Greenit</h1>
-      <h2
-        className={
-          enlightenment === "Sign in" ? "header__signin" : "header__signin free"
-        }
-        onClick={proclaimFreedom}
-      >
-        {enlightenment}
-      </h2>
     </div>
   );
 };
